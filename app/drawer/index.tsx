2c5a2b228ec8e622d7cee30b6bc0fb9ef3b9864c
demichelisgimenez <demichelisgimenez@gmail.com>
2025-09-25 18:25:27 -0300

    iconos drawer

--- app/drawer/index.tsx
+++ app/drawer/index.tsx
@@ -11,6 +11,8 @@ import Ajustes from "@app/screens/ajustes";
 import Login from "@app/auth/screens/login";
 import { Register } from "@app/auth/screens";
 
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+
 const Drawer = createDrawerNavigator();
 
 export default function DrawerNavigator() {
@@ -19,24 +21,88 @@ export default function DrawerNavigator() {
       <Drawer.Screen
         name={DRAWER_ROUTES.INICIO}
         component={InicioScreen}
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          drawerIcon: ({ color, size }) => (
+            <MaterialIcons name="home" size={size} color={color} />
+          ),
+        }}
       />
-      <Drawer.Screen name={DRAWER_ROUTES.FARMACIAS} component={Farmacias} />
-      <Drawer.Screen name={DRAWER_ROUTES.ESCUELAS} component={Escuelas} />
-      <Drawer.Screen name={DRAWER_ROUTES.RADIOS} component={Radios} />
-      <Drawer.Screen name={DRAWER_ROUTES.NOTAS} component={Notas} />
-      <Drawer.Screen name={DRAWER_ROUTES.QR} component={QR} />
-      <Drawer.Screen name={DRAWER_ROUTES.AJUSTES} component={Ajustes} />
+      <Drawer.Screen
+        name={DRAWER_ROUTES.FARMACIAS}
+        component={Farmacias}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <MaterialIcons name="local-pharmacy" size={size} color={color} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name={DRAWER_ROUTES.ESCUELAS}
+        component={Escuelas}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <MaterialIcons name="school" size={size} color={color} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name={DRAWER_ROUTES.RADIOS}
+        component={Radios}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <MaterialIcons name="radio" size={size} color={color} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name={DRAWER_ROUTES.NOTAS}
+        component={Notas}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <MaterialIcons name="note-alt" size={size} color={color} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name={DRAWER_ROUTES.QR}
+        component={QR}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <MaterialIcons name="qr-code-scanner" size={size} color={color} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name={DRAWER_ROUTES.AJUSTES}
+        component={Ajustes}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <MaterialIcons name="settings" size={size} color={color} />
+          ),
+        }}
+      />
       <Drawer.Screen
         name={AUTH_ROUTES.LOGIN}
         component={Login}
-        options={{ title: "Iniciar Sesión" }}
+        options={{
+          title: "Iniciar Sesión",
+          drawerIcon: ({ color, size }) => (
+            <MaterialIcons name="login" size={size} color={color} />
+          ),
+        }}
       />
 
       <Drawer.Screen
         name={AUTH_ROUTES.REGISTER}
         component={Register}
-        options={{ title: "Registrarse", drawerItemStyle: { display: "none" }}}
+        options={{
+          title: "Registrarse",
+          drawerItemStyle: { display: "none" },
+          drawerIcon: ({ color, size }) => (
+            <MaterialIcons name="person-add" size={size} color={color} />
+          ),
+        }}
       />
     </Drawer.Navigator>
   );
